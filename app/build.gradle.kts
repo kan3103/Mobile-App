e60dc4e7e5106b82596808cbddba6cf170cedf2c
Nam Nguyen <meomaythongminh3@gmail.com>
2024-04-03 15:31:54 +0700

    Add list of doctors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,9 @@ android {
 }
 
 dependencies {
+
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
---
+++ app/src/main/java/com/example/mobile_app/Data/Doctor.java
@@ -0,0 +1,48 @@
+package com.example.mobile_app.Data;
+
+public class Doctor extends Person {
+    private int doctorFees;
+    private String degree;
+    private String specialization;
+
+    Doctor(){}
+
+    public Doctor(String id,String firstname,String lastName,String emailAddress,String password,String mobile,String Address,String doctorFees,String degree,String specialization){
+        this.setId(id);
+        this.setUsertype(Macros.DOCTOR);
+        this.firstName=firstname;
+        this.lastName=lastName;
+        this.emailAddress=emailAddress;
+        this.password=password;
+        this.mobileNumber=mobile;
+        this.address=emailAddress;
+        this.doctorFees=Integer.valueOf(doctorFees);
+        this.degree=degree;
+        this.specialization=specialization;
+    }
+
+    public int getDoctorFees() {
+        return doctorFees;
+    }
+
+    public void setDoctorFees(int doctorFees) {
+        this.doctorFees = doctorFees;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+}
+
