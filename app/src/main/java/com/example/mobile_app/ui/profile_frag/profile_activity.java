fd1f711daa911aac379a25580a5e42d28871104a
Btbach25 <bach.buithanh40@hcmut.edu.vn>
2024-04-05 15:29:34 +0700

    Something

---
+++ app/src/main/java/com/example/mobile_app/ui/profile_frag/profile_activity.java
@@ -0,0 +1,16 @@
+package com.example.mobile_app.ui.profile_frag;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.something.R;
+
+public class profile_activity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.profile_layout);
+    }
+}
