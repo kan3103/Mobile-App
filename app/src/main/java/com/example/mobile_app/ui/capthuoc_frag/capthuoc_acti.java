3920ea5ba0451bc4ac80fbfec40e0e28de2e596e
Btbach25 <bach.buithanh40@hcmut.edu.vn>
2024-04-15 14:12:07 +0700

    sua file 1 chut

--- app/src/main/java/com/example/mobile_app/ui/capthuoc_frag/capthuoc_acti.java
+++ app/src/main/java/com/example/mobile_app/ui/capthuoc_frag/capthuoc_acti.java
@@ -3,6 +3,7 @@ package com.example.mobile_app.ui.capthuoc_frag;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -15,34 +16,44 @@ import java.util.ArrayList;
 public class capthuoc_acti extends AppCompatActivity {
         Button button;
         ArrayList<String> tenthuoc ;
-        @Override
+        ListView listView ;
+        EditText id_benhnhan;
+    @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             EdgeToEdge.enable(this);
             setContentView(R.layout.capthuoc_layout);
 
-//            tenthuoc.add("Penicilin") ;
-//            tenthuoc.add("Paracetamol") ;
-//            tenthuoc.add("Coldacmin") ;
-//            tenthuoc.add("Panadol") ;
-//            tenthuoc.add("Vitamin B1") ;
-//
-//            tenthuoc.add("Petol") ;
-//            tenthuoc.add("ROVAS") ;
-//            tenthuoc.add("Franvit") ;
-//            tenthuoc.add("Avamys") ;
-//            tenthuoc.add("Prospan") ;
-//
-//            capthuoc_adap adapter = new capthuoc_adap(this,R.layout.capthuoc_listview_custom, tenthuoc) ;
-//            ListView listView = findViewById(R.id.list_view_capthuoc) ;
-//            listView.setAdapter(adapter);
-//            button.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//
-//                }
-//            });
+            tenthuoc.add("Penicilin") ;
+            tenthuoc.add("Paracetamol") ;
+            tenthuoc.add("Coldacmin") ;
+            tenthuoc.add("Panadol") ;
+            tenthuoc.add("Vitamin B1") ;
 
+            tenthuoc.add("Petol") ;
+            tenthuoc.add("ROVAS") ;
+            tenthuoc.add("Franvit") ;
+            tenthuoc.add("Avamys") ;
+            tenthuoc.add("Prospan") ;
+            id_benhnhan = findViewById(R.id.idbenhnhan_capthuoc) ;
+            capthuoc_adap adapter = new capthuoc_adap(this,R.layout.capthuoc_listview_custom, tenthuoc) ;
+            listView = findViewById(R.id.list_view_capthuoc) ;
+            listView.setAdapter(adapter);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+        }
+        public void getChild(){
+            for (int i = 0; i < listView.getChildCount(); i++) {
+                View rowView = listView.getChildAt(i);
+                EditText editText = (EditText) rowView.findViewById(R.id.edittext_capthuoc);
+                String text = editText.getText().toString();
+
+            }
         }
 
 }
--- app/src/main/java/com/example/mobile_app/ui/profile_frag/profile_activity.java
+++ app/src/main/java/com/example/mobile_app/ui/profile_frag/profile_activity.java
@@ -1,15 +1,100 @@
 package com.example.mobile_app.ui.profile_frag;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.mobile_app.LoginActivity;
+import com.example.mobile_app.MainActivity;
 import com.example.mobile_app.R;
+import com.example.mobile_app.api.user.factoryUser.Login;
+import com.example.mobile_app.api.user.userObject.adminUser;
+import com.example.mobile_app.api.user.userObject.userInterface;
+
+import org.w3c.dom.Document;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import io.realm.Realm;
+import io.realm.mongodb.App;
+import io.realm.mongodb.AppConfiguration;
+import io.realm.mongodb.Credentials;
+import io.realm.mongodb.User;
+import io.realm.mongodb.mongo.MongoClient;
+import io.realm.mongodb.mongo.MongoCollection;
+import io.realm.mongodb.mongo.MongoDatabase;
+
 public class profile_activity extends AppCompatActivity {
+
+    String Appid = "mobileapp-fyjbw";
+    MongoDatabase mongoDatabase;
+    MongoClient mongoClient;
+    MongoCollection<Document> mongoCollection;
+    userInterface user;
+    User anotheruser;
+    String userName ;
+    ArrayList<Item_list> items ;
+    ListView listView ;
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.profile_layout);
+
+        Intent i = getIntent();
+        if (i != null) {
+            user = (userInterface) i.getSerializableExtra("userobject");
+            userName = user.getUsername();
+        }
+
+
+        Realm.init(getApplicationContext());
+        App app = new App(new AppConfiguration.Builder(Appid).build());
+        Credentials credentials = Credentials.emailPassword("khanglytronVN@KL.com", "123456");
+        app.getEmailPassword().registerUserAsync("khanglytronVN@KL.com", "123456", it -> {
+            if (it.isSuccess()) {
+                Log.v("User", "Successfully registered user");
+            } else {
+                Log.v("User", "Failed to register user");
+            }
+        });
+        app.loginAsync(credentials, new App.Callback<User>() {
+            @Override
+            public void onResult(App.Result<User> result) {
+                anotheruser = app.currentUser();
+                mongoClient = anotheruser.getMongoClient("mongodb-atlas");
+                mongoDatabase = mongoClient.getDatabase("Hospital");
+                mongoCollection = mongoDatabase.getCollection("Patient");
+            }
+        });
+        Document doc;
+        org.bson.Document document = new org.bson.Document().append("username",userName);
+        mongoCollection.findOne(document).getAsync(result -> {
+            if (result.isSuccess()) {
+                doc = result.get();
+
+            } else {
+                Toast.makeText(getApplicationContext(), "FAIL", Toast.LENGTH_LONG).show();
+            }
+        });
+        items = new ArrayList<>();
+        ArrayList<String> keys ;
+        keys = new ArrayList<>(Arrays.asList("name","sex", "userName", "phoneNum", "birthday", "nationality", "address", "occupation" ));
+        for(int index = 0 ; index < 8; index++ ){
+            String descrip = doc.getString(keys[index]);
+
+            items.add(new Item_list(keys[index] , descrip));
+        }
+
+        profile_adap adapter = new profile_adap(items, getApplicationContext() );
+        listView.setAdapter(adapter);
+
     }
 }
+
